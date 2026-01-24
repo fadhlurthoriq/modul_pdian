91f6c37c14decd9c5eaa2db0aae886917919f0aa
fadhlurthoriq <muhammadfthoriq@gmail.com>
2026-01-24 07:44:23 +0700

    mod 7 firebase auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:project_chat/auth/login_or_register.dart';
+import 'package:project_chat/firebase_options.dart';
 import 'package:project_chat/themes/light_mode.dart';
 
-void main(){
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
   runApp(const MyApp());
 }
 
